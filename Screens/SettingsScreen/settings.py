6406657c80f91bcf4aded9328a9d6335a0e5aeac
Richard Souza <souzza.richard25@gmail.com>
2023-01-19 16:14:10 -0300

    Fix on_touch_up bug
    Optimization and fixing bug in the 'on_touch_up' event that occurred after building the application.

--- Screens/SettingsScreen/settings.py
+++ Screens/SettingsScreen/settings.py
@@ -7,6 +7,8 @@ class SettingsScreen(MDScreen):
         super(SettingsScreen, self).__init__(**kwargs)
         self.app = MDApp.get_running_app()
     
-    def set_board_size(self, size):
-        self.app.root.get_screen("game").build(board_size=size)
-        
+    def set_board_size(self):
+        game = self.app.root.get_screen("game")
+        value = self.ids["slider"].value
+        if value != game.board_size:
+            game.build(board_size=value)

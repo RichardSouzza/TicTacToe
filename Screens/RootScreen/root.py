817b848627d91164fbde718c0cee021af6c4cce0
Richard Souza <souzza.richard25@gmail.com>
2024-10-11 16:48:13 -0300

    feat: implements handler for return action

--- Screens/RootScreen/root.py
+++ Screens/RootScreen/root.py
@@ -1,7 +1,15 @@
+from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+from kivy.clock import Clock
+from kivy.core.window import Window
 
 
 class RootScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(RootScreen, self).__init__(**kwargs)
+        self.app = MDApp.get_running_app()
+        Clock.schedule_once(lambda x: Window.bind(on_keyboard=self.handle_actions))
+
     def change_screen(self, screen, direction="left"):
         self.ids.screen_manager.transition.direction = direction
         self.ids.screen_manager.current = screen
@@ -12,3 +20,9 @@ class RootScreen(MDScreen):
     
     def get_screen(self, name):
         return self.ids.screen_manager.get_screen(name)
+    
+    def handle_actions(self, window, key, *args):
+        if key == 27: # Esc
+            if self.current_screen.name != "home":
+                self.change_screen("home", "right")
+                return True
